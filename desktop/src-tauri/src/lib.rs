91426371076246d4cf4626cd9b261d412010b613
andrejjelenkovic5-cmd <andrej.jelenkovic5@gmail.com>
2026-09-03 14:36:37 +0200

    Update nove funkcije

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use tauri_plugin_opener::OpenerExt;
+use tauri_plugin_notification::NotificationExt;
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,13 +12,22 @@ fn open_external_link(app: tauri::AppHandle, url: String) -> Result<(), String> 
     app.opener().open_url(url, None::<String>).map_err(|e| e.to_string())
 }
 
+#[tauri::command]
+fn send_desktop_notification(app: tauri::AppHandle, title: String, body: String) -> Result<(), String> {
+    app.notification()
+        .builder()
+        .title(title)
+        .body(body)
+        .show()
+        .map_err(|e| e.to_string())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(tauri_plugin_process::init())
-        .invoke_handler(tauri::generate_handler![greet, open_external_link])
+        .plugin(tauri_plugin_notification::init())
+        .invoke_handler(tauri::generate_handler![greet, open_external_link, send_desktop_notification])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
